702b3cc4fa0d8e505f793c6784900ff13901c492
chenfeng <>
2021-10-18 17:34:27 +0800

    加入一个工具类和优化了一下启动完成日志打印

--- src/main/java/com/uxunchina/taling/system/controller/HelloController.java
+++ src/main/java/com/uxunchina/taling/system/controller/HelloController.java
@@ -5,6 +5,7 @@ import com.ctrip.framework.apollo.spring.annotation.ApolloConfig;
 import com.uxunchina.taling.common.utils.RedisUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,7 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.naming.Name;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -53,7 +61,9 @@ public class HelloController {
         String value = (String)redisTemplate.opsForValue().get("key1");
         String value2 = (String)redisUtils.get("key2");
         log.warn("value1={},value2={}" ,value,value2);
-
+        redisUtils.set("cf",1);
+        redisUtils.incr("cf",100);
+        log.info("redis Key:cf value:{}",redisUtils.get("cf"));
         Set<String> propertyNames = config.getPropertyNames();
         propertyNames.forEach(key -> {
             log.info("{}={}",key,config.getProperty(key,"123"));
@@ -63,4 +73,49 @@ public class HelloController {
         return propertyNames.toString()+name;
     }
 
+    public static String strSpecialFilter(String str) {
+        String regEx = "[\\u00A0\\s\"`~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}《》【】‘；：”“’。，、？]";
+        Pattern p = Pattern.compile(regEx);
+        Matcher m = p.matcher(str);
+        //将所有的特殊字符替换为空字符串
+        return m.replaceAll("").trim();
+    }
+
+    public static Integer getRemainSecondsOneDay(Date currentDate) {
+        Calendar midnight=Calendar.getInstance();
+        midnight.setTime(currentDate);
+        midnight.add(midnight.DAY_OF_MONTH,1);
+        midnight.set(midnight.HOUR_OF_DAY,0);
+        midnight.set(midnight.MINUTE,0);
+        midnight.set(midnight.SECOND,0);
+        midnight.set(midnight.MILLISECOND,0);
+        Integer seconds=(int)((midnight.getTime().getTime()-currentDate.getTime())/1000);
+        return seconds;
+    }
+
+    public static Integer getRemainSecondsOneDay2(Date currentDate) {
+        LocalDateTime midnight = LocalDateTime.ofInstant(currentDate.toInstant(),
+                ZoneId.systemDefault()).plusDays(1).withHour(0).withMinute(0)
+                .withSecond(0).withNano(0);
+        LocalDateTime currentDateTime = LocalDateTime.ofInstant(currentDate.toInstant(),
+                ZoneId.systemDefault());
+        long seconds = ChronoUnit.SECONDS.between(currentDateTime, midnight);
+        return (int) seconds;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(strSpecialFilter("大河明珠'1号楼<顺达标准件>《鲁迅收》[word]"));
+        System.out.println(getRemainSecondsOneDay2(new Date()));
+
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        //加密所需的salt(盐)
+        textEncryptor.setPassword("chenfeng");
+        //要加密的数据（数据库的用户名或密码）
+        String username = textEncryptor.encrypt("root");
+        String password = textEncryptor.encrypt("123456");
+        System.out.println("username:"+username);
+        System.out.println("password:"+password);
+
+    }
 }
