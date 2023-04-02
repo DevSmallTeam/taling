9868a8b86e14cc8b3f23c1209e298330d6074b88
chenfeng <844474380@qq.com>
2023-04-02 17:04:53 +0800

    websocket domo

---
+++ src/main/java/org/chenfeng/taling/common/configure/WebSocketConfigure.java
@@ -0,0 +1,24 @@
+package org.chenfeng.taling.common.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * webSocket config
+ *
+ * @author chenfeng
+ * @date 2022/04/11 16:37
+ **/
+@Configuration
+public class WebSocketConfigure {
+
+    /**
+     * 	注入ServerEndpointExporter，
+     * 	这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+     */
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
